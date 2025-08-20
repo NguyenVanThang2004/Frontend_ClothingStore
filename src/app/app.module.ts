28dd309252e452f208333e8744f854911833cc65
NguyenVanThang2004 <thangdepzai38@gmail.com>
2025-08-20 18:09:38 +0700

    create UI blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CheckoutComponent } from './checkout/checkout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ShopComponent } from './shop/shop.component';
     CheckoutComponent,
     LoginComponent,
     RegisterComponent,
-    ShopComponent
+    ShopComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +38,6 @@ import { ShopComponent } from './shop/shop.component';
     FormsModule
   ],
   providers: [],
-  bootstrap: [ShopComponent]
+  bootstrap: [BlogComponent]
 })
 export class AppModule { }
